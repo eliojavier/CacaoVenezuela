1ead0d78d17c1768d8e2c0048e298bc0df3b52c9
Elio Acosta <eliojavier86@gmail.com>
2016-12-27 15:02:33 -0430

    arreglos

--- resources/views/admin/votes/index.blade.php
+++ resources/views/admin/votes/index.blade.php
@@ -31,6 +31,31 @@
                                     @endforeach
                                 </td>
                                 <td>{{$recipe->preparation}}</td>
+                                {{--<td>--}}
+                                    {{--<a href="{{ url('admin/recetas/' . $recipe->id . '/edit') }}">--}}
+                                        {{--<button type="button" class="btn btn-primary">--}}
+                                            {{--<i class="fa fa-pencil-square-o" aria-hidden="true"></i>--}}
+                                        {{--</button>--}}
+                                    {{--</a>--}}
+                                    {{--<button type="button" id="edit" data-toggle="modal" data-target="#editar-juez" class="btn btn-warning">--}}
+                                    {{--<span class="glyphicon glyphicon-edit"></span>--}}
+                                    {{--</button>--}}
+                                    {{--<button type="button" data-toggle="modal" data-target="#eliminar-juez" class="btn btn-danger">--}}
+                                    {{--<span class="glyphicon glyphicon-trash"></span>--}}
+                                    {{--</button>--}}
+                                {{--</td>--}}
+                                {{--<td>--}}
+                                    {{--<a href="{{ url('admin/jueces/' . $judge->id . '/edit') }}">--}}
+                                    {{--<button type="button" class="btn btn-danger">--}}
+                                    {{--<span class="glyphicon glyphicon-edit"></span>Eliminar--}}
+                                    {{--<i class="fa fa-trash-o" aria-hidden="true"></i>--}}
+                                    {{--<i class="fa fa-trash-o" aria-hidden="true"></i>--}}
+                                    {{--</button>--}}
+                                    {{--</a>--}}
+                                    {{--{!! Form::open(['method' => 'DELETE', 'url' => 'admin/recetas/' . $recipe->id, 'class'=>'form-horizontal', 'role'=>'form']) !!}--}}
+                                        {{--{!!Form::button('Eliminar', ['type'=>'button', 'class'=>'btn btn-primary'])!!}--}}
+                                    {{--{!!Form::close()!!}--}}
+                                {{--</td>--}}
                             </tr>
                         </tbody>
                     </table>
@@ -40,6 +65,15 @@
         </div>
         <div class="row">
             <div class="col-md-4">
+                @foreach($criteria as $criterion)
+                    <div class="form-group">
+                        {!!Form::label('criterion',$criterion->criterion)!!}
+                        {!!Form::select($criterion->id, $scores, null, ['class' => 'form-control'])!!}
+                    </div>
+                @endforeach
+                <div class="form-group">
+                    {!!Form::submit('Votar', ['class' => 'btn btn-primary'])!!}
+                </div>
                 {{ $recipes->links() }}
             </div>
         </div>
@@ -47,3 +81,12 @@
         {!!Form::close()!!}
     </div>
 @endsection
+{{--@section('scripts')--}}
+{{--<script>--}}
+{{--$(document).on("click", "#edit", function () {--}}
+{{--var row = $(this).closest('tr').attr('id');--}}
+{{--$(".modal-body #id").val(row);--}}
+{{--});--}}
+{{--</script>--}}
+
+{{--@endsection--}}
